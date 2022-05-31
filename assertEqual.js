3c13826f6330f360b8a037c092e10bf94cdc7a72
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-31 03:07:38 +0000

    flipped the emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
 const assertEqual = function (actual, expected) {
-  if (actual === expected); {
-    console.log("🛑 🛑 🛑 Assertion Passed: " + "[" + actual + "] ===  [" + expected + "]");
-  } if (actual !== expected); {
-    console.log("✅ ✅ ✅ Assertion Failed: [" + actual + "] !==  [" + expected + "]");
+  if (actual === expected) {
+    console.log("✅ ✅ ✅ Assertion Passed: " + "[" + actual + "] ===  [" + expected + "]");
+  } if (actual !== expected) {
+    console.log("🛑 🛑 🛑 Assertion Failed: [" + actual + "] !==  [" + expected + "]");
   }
 };
+
 assertEqual(4,4);
 assertEqual(4,3);
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp"); 
+
