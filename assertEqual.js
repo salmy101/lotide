89df472abf185aa0215e4b3969d9238f2ce8f454
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-31 02:52:37 +0000

    made a custom function to tell me if we failed the assertion or not

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected); {
+    console.log("🛑 🛑 🛑 Assertion Passed: " + "[" + actual + "] ===  [" + expected + "]");
+  } if (actual !== expected); {
+    console.log("✅ ✅ ✅ Assertion Failed: [" + actual + "] !==  [" + expected + "]");
+  }
+};
+assertEqual(4,4);
+assertEqual(4,3);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
