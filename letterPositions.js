c8ef019c24d91003b5b6afc2acbf97f0335521cd
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-02 17:21:26 +0000

    used a for-in loop, use what mr.park taught me to acess keys and values!

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+// const letterPositions = function(sentence) {
+//   const results = {};
+//   for(const i in sentence)
+//   if(results[sentence[i]]){
+//     results[sentence[i]] = i
+//     console.log(i)
+
+//   }
+//   return results;
+// };
+
+const letterPositions = function(string){
+  const results = {}
+  for(const index in string) { //loop through and get the index of every letter
+  //console.log(index) this check give me index
+  //console.log(string[index]) this check gives me the letters
+    let key = string[index]  //THEREFORE set key variable to string[index] (the letters) = and call vlaue with bracket notation results[key]
+  if(results[key]){ //if the value exists...in the results object
+    results[key] += 1 // increase by one
+  } if(results[key] === " "){
+   continue; //this is suppose to skip the " "
+  } else { //else...aka doenst already exist in the object
+    results[key] = 1 //start it at 1
+  }
+  
+  }
+  return results;
+}
+
+
+
+
+
+console.log(letterPositions("Salma is"));
