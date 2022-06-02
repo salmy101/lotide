3e0e2b5d2dbe1e32063840d96ef7a9e7b1a11f56
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-02 02:48:43 +0000

    done

--- countOnly.js
+++ countOnly.js
@@ -7,15 +7,21 @@ const assertEqual = function (actual, expected) {
 };
 // write countOnly function below
 
+
+   
 const countOnly = function(allItems, itemsToCount) {
-  const results = {}
+  const results = {};
   for (const item of allItems) {
-    console.log(item);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
   }
-
   return results;
-
-}
+};
 
 
 const firstNames = [
