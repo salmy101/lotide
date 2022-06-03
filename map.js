877bd7c922e1e389427b7fff4c4eb9d6596645e6
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-03 05:17:28 +0000

    Initial commit

--- map.js
+++ map.js
@@ -1,23 +1,4 @@
-const eqArrays = function (ar1, ar2) {
-  if(ar1.length !== ar2.length) {
-    return false;
-  }
-  for (var i = 0, l=ar1.length; i < l; i++) {
-  if (ar1[i] !== ar2[i]){
-      return false;  
-    }  
-  }
-  return true
-} 
-
-const assertArraysEqual = function(actual,expected){
-  let eqvArrays = eqArrays(actual,expected)
-  if(eqvArrays === false) {
-    console.log(`🛑 🛑 🛑 Assertion Failed:[${actual}] !== [${expected}]`);
-  } if(eqvArrays === true){
-    console.log(`✅ ✅ ✅ Assertion Passed:[${actual}] === [${expected}]`);
-  }
-}  
+ 
 
 
 
