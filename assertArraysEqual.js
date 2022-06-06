6ec5961ef17fcc25a6d42424ab556e837c68e263
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-06 21:57:04 +0000

    refactored and modualrized the necessary functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function (ar1, ar2) {
-  if(ar1.length !== ar2.length) {
-    return false;
-  }
-  for (var i = 0, l=ar1.length; i < l; i++) {
-  if (ar1[i] !== ar2[i]){
-      return false;  
-    }  
-  }
-  return true
-} 
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = function(actual,expected){
   let eqvArrays = eqArrays(actual,expected)
@@ -19,6 +9,4 @@ const assertArraysEqual = function(actual,expected){
   }
 }   
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
-assertArraysEqual([5,6,3,7],[5,6,3,7,9])
+module.exports = assertArraysEqual
